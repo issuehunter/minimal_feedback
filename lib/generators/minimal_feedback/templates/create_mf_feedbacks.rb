5cc68a8de8ea3c57fd0ca53c6310c70df5cf45cf
Matteo Depalo <matteodepalo@gmail.com>
2013-01-11 19:45:18 +0100

    Removed rating column

--- lib/generators/minimal_feedback/templates/create_mf_feedbacks.rb
+++ lib/generators/minimal_feedback/templates/create_mf_feedbacks.rb
@@ -2,7 +2,6 @@ class CreateMfFeedbacks < ActiveRecord::Migration
   def change
     create_table :mf_feedbacks do |t|
       t.string :type
-      t.integer :rating
       t.references :user
       t.references :rateable, :polymorphic => true
       t.timestamps
